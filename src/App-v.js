5d8fea969b3a9f55017a07523c5ac11c24170c52
Sravya Tanniru <sravyatanniru@gmail.com>
2023-12-12 17:14:34 -0600

    initial commit

---
+++ src/App-v.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Logo from "./components/Logo";
+import Form from "./components/Form";
+import "./index.css";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItems(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all the items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItems}
+        onToggleItem={handleToggleItems}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
